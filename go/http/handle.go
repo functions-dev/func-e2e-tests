6b0b9312a7928c3f061990f8a41e6bb251191ebc
Luke Kingland <luke@lukekingland.com>
2024-05-06 16:33:52 +0900

    update commments

--- go/http/handle.go
+++ go/http/handle.go
@@ -7,8 +7,8 @@ import (
 
 // Handle an HTTP Request.
 func Handle(w http.ResponseWriter, r *http.Request) {
-	// The E2E test of the same name checks for this exact string in the
-	// HTTP response to confirm the function was both initialized using this
-	// template and successfully deployed.
+	// The E2E test checks for this exact string in the HTTP response to
+	// confirm the function was both initialized using this template and
+	// successfully deployed.
 	fmt.Fprintln(w, "func-e2e-test-deploy-template")
 }
--- handle.go
+++ handle.go
@@ -7,5 +7,8 @@ import (
 
 // Handle an HTTP Request.
 func Handle(w http.ResponseWriter, r *http.Request) {
+	// The E2E test checks for this exact string in the HTTP response to
+	// confirm the function was both initialized using this template and
+	// successfully deployed.
 	fmt.Fprintln(w, "func-e2e-test-deploy-source")
 }
