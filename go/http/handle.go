26910e730b72d8c2a83b3d211d34a5a40047b167
Luke Kingland <luke@lukekingland.com>
2025-05-29 14:54:46 +0900

    update naming

--- go/http/handle.go
+++ go/http/handle.go
@@ -10,5 +10,5 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	// The E2E test checks for this exact string in the HTTP response to
 	// confirm the function was both initialized using this template and
 	// successfully deployed.
-	fmt.Fprintln(w, "func-e2e-test-deploy-template")
+	fmt.Fprintln(w, "func-e2e-test-core-deploy-template")
 }
