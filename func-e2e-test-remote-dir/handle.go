db8f79fb0106947c24dfe14b2278a8500dc03733
Luke Kingland <luke@lukekingland.com>
2025-06-11 17:06:09 +0900

    naming updates and subdir function

--- func-e2e-test-remote-dir/handle.go
+++ func-e2e-test-remote-dir/handle.go
@@ -8,6 +8,6 @@ import (
 // Handle an HTTP Request.
 func Handle(w http.ResponseWriter, r *http.Request) {
 	// The E2E test checks for this exact string in the HTTP response to
-	// confirm the function was deployed from this remote repository.
-	fmt.Fprintln(w, "func-e2e-test-remote-source")
+	// confirm the function from this subdirectory was deployed.
+	fmt.Fprintln(w, "func-e2e-test-remote-dir")
 }
