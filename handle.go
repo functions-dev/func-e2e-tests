34d96245ac61c8db9e20ff34994d0b0b6ad712ad
Luke Kingland <luke@lukekingland.com>
2025-06-11 16:02:39 +0900

    test modification

--- handle.go
+++ handle.go
@@ -10,5 +10,8 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	// The E2E test checks for this exact string in the HTTP response to
 	// confirm the function was both initialized using this template and
 	// successfully deployed.
-	fmt.Fprintln(w, "func-e2e-test-remote-deploy-source")
+	//
+	// Modified in this branch only in order to test deployments from
+	// a specific reference
+	fmt.Fprintln(w, "func-e2e-test-remote-ref")
 }
